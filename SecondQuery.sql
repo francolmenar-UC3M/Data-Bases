f748e2fd5d1e6c857bee51add262d910eafa3020
francolmenar <correofalso97@hotmail.es>
2017-04-02 18:26:54 +0200

    Add files via upload

--- SecondQuery.sql
+++ SecondQuery.sql
@@ -2,39 +2,34 @@
 -- -- Creation of the Top 5 stars involved in more   --
 -- -- movies from USA --------------------------------
 -- ----------------------------------------------------
-
-DROP VIEW mostUSA;
-
-CREATE VIEW mostUSA AS(
-SELECT actor,
-  rank() OVER (ORDER BY
-mostUSA2) desc)
-AS top_star
-FROM CASTS
-where top_star <= 5);
-
-SELECT * FROM mostUSA;
-
-
-
 DROP VIEW join1;
 
 CREATE VIEW join1 AS
-SELECT actor, movie_title FROM
-CASTS NATURAL JOIN (MOVIES
-WHERE country='USA');
+SELECT DISTINCT actor, movie_title FROM
+CASTS NATURAL JOIN MOVIES
+WHERE country='USA';
 
 SELECT * FROM join1;
 
 
+DROP VIEW group1;
+
+CREATE VIEW group1 AS
+SELECT actor, count('x') top
+FROM join1
+GROUP BY actor;
+
+SELECT * FROM group1;
 
 
-DROP VIEW mostUSA2;
 
-CREATE VIEW mostUSA2 AS(
-SELECT actor, count('x') top FROM(
-CASTS NATURAL JOIN (MOVIES
-WHERE country='USA'))
-GROUP BY actor);
+DROP VIEW mostUSA;
 
-SELECT * FROM mostUSA2;
+CREATE VIEW mostUSA AS
+SELECT actor,  rank() OVER (
+ORDER BY top) desc)
+AS top_star
+FROM group1
+where top_star <= 5);
+
+SELECT * FROM mostUSA;
