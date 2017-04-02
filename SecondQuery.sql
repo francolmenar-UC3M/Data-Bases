cd079cee32541c2db5f63e1d96a9614723f96f33
francolmenar <correofalso97@hotmail.es>
2017-04-02 12:58:40 +0200

    Update SecondQuery.sql

--- SecondQuery.sql
+++ SecondQuery.sql
@@ -2,18 +2,18 @@
 -- -- Creation of the Top 5 stars involved in more   --
 -- -- movies from USA --------------------------------
 -- ----------------------------------------------------
-
 DROP VIEW mostUSA;
 
 CREATE VIEW mostUSA AS
-	SELECT * FROM(
-	SELECT actor,
-  rank() OVER (ORDER BY
-    (SELECT actor_name FROM
-    CASTS NATURAL JOIN MOVIES
-    GROUP BY actor_name) desc)
-  AS top_star
-	FROM CAST)
-	where top_star <= 5;
+
+SELECT * FROM(
+SELECT actor,
+  rank() OVER (ORDER BY    
+(SELECT actor FROM    
+CASTS NATURAL JOIN MOVIES    
+GROUP BY actor) desc)  
+AS top_star	
+FROM CASTS)	
+where top_star <= 5;
 
 SELECT * FROM mostUSA;
