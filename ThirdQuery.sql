5dfbc78cfeba5799e53976193d4bbb3375da21f2
gabrielglbh <100346140@alumnos.uc3m.es>
2017-04-03 17:26:17 +0200

    Change Yet

--- ThirdQuery.sql
+++ ThirdQuery.sql
@@ -1,10 +1,10 @@
 SELECT name, surname, SEASONS.title, SEASONS.season
-  FROM (SEASONS JOIN lic_series ON (SEASONS.episodes=lic_series.episode AND SEASONS.season=lic_series.season)
+  FROM (SEASONS JOIN lic_series ON SEASONS.season=lic_series.season)
        JOIN clients ON clientId=client)
-  GROUP BY SEASONS.title HAVING COUNT(lic_series.season)=max(SEASONS.episode);
+  GROUP BY SEASONS.title HAVING COUNT(lic_series.episode)=max(SEASONS.episode);
   
 --SELECT COUNT(*) FROM (..);  
---279446--
+
   
 
 
