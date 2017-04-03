923f4a06c878606b8886fe53458ec4e7bf8ccb5e
gabrielglbh <100346140@alumnos.uc3m.es>
2017-04-03 15:49:58 +0200

    Update FirstQuery.sql

--- FirstQuery.sql
+++ FirstQuery.sql
@@ -1,22 +1,3 @@
--- ----------------------------------------------------
--- -- Creation of the projection of name and surname --
--- -- from client order by surname,name ---------------
--- ----------------------------------------------------
-DROP TABLE current_clients;
-
-CREATE TABLE current_clients(
-clientId	VARCHAR2(15),
-name		VARCHAR2(100) NOT NULL,
-surname		VARCHAR2(100) NOT NULL,
-startdate DATE NOT NULL,
-enddate DATE,
-type VARCHAR2(1),
-CONSTRAINT PK_current_clients PRIMARY KEY (clientId),
-CONSTRAINT FK_current_clients1 FOREIGN KEY (clientId) REFERENCES clients,
-CONSTRAINT CK_current_clients6 CHECK (type IN ('C','V'))
-);
-
-
  INSERT INTO current_clients
 	SELECT clientId, name, surname, startdate, enddate, type
 	FROM 
@@ -29,5 +10,3 @@ CONSTRAINT CK_current_clients6 CHECK (type IN ('C','V'))
 	WHERE (sysdate BETWEEN enddate AND startdate) OR (sysdate => startdate AND enddate IS NULL))
 	       JOIN products ON product_name=contract_type;
 --2258--
-
-SELECT * FROM current_clients;
