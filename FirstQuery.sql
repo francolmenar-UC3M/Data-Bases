087f91f4d3ff0ef42981b6d38eec359eb46e1e00
gabrielglbh <100346140@alumnos.uc3m.es>
2017-04-03 15:35:47 +0200

    Update FirstQuery.sql

--- FirstQuery.sql
+++ FirstQuery.sql
@@ -18,14 +18,16 @@ CONSTRAINT FK_current_clients6 FOREIGN KEY (contract_type) REFERENCES products
 
 
  INSERT INTO current_clients
-	SELECT clientId, name, surname, startdate, enddate, contract_type
-	FROM ((
-	SELECT name, surname , clientId
+	SELECT clientId, name, surname, startdate, enddate, type
+	FROM 
+	(SELECT name, surname , clientId
 	FROM clients
-	ORDER BY surname, name) NATURAL JOIN (
-		SELECT startdate, enddate, contract_type, clientId
+	ORDER BY surname, name) 
+	NATURAL JOIN 
+	(SELECT startdate, enddate, contract_type, clientId
 	FROM contracts
-	WHERE ((sysdate BETWEEN enddate AND startdate) OR (enddate IS NULL))));
---2140--
+	WHERE ((sysdate BETWEEN enddate AND startdate) OR (sysdate > startdate AND enddate IS NULL)))
+	       JOIN products ON product_name=contract_type;
+--2258--
 
 SELECT * FROM current_clients;
