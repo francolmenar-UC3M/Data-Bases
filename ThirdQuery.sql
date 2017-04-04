26a517987b21384c8723818b27bce820c63fb444
gabrielglbh <100346140@alumnos.uc3m.es>
2017-04-04 11:16:32 +0200

    Update ThirdQuery.sql

--- ThirdQuery.sql
+++ ThirdQuery.sql
@@ -1,6 +1,6 @@
 SELECT name, surname, SEASONS.title, SEASONS.season
-  FROM (SELECT season, title, episode FROM SEASONS 
-        JOIN SELECT season, title, episodes FROM lic_series ON SEASONS.season=lic_series.season AND SEASONS.title=lic_series.title)
+  FROM ((SELECT season, title, episode FROM SEASONS) 
+        JOIN (SELECT season, title, episodes FROM lic_series) ON SEASONS.season=lic_series.season AND SEASONS.title=lic_series.title)
        JOIN clients ON clientId=client
   GROUP BY SEASONS.season HAVING MAX(episode)=episode;
   
