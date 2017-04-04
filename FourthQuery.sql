01609cdbe51e27749c9423cc041c0a83cd76f44f
Gabriel García <100346140@alumnos.uc3m.es>
2017-04-04 15:52:57 +0200

    Update FourthQuery.sql

--- FourthQuery.sql
+++ FourthQuery.sql
@@ -1,8 +1,22 @@
 SELECT actor, month, max
        FROM (SELECT month, MAX(*) FROM
-              (SELECT actor, to_char(view_datetime, 'MON-YYYY') AS month, COUNT(*) FROM CASTS JOIN taps_movies ON CASTS.title=taps_movies.title  
-               GROUP BY actor, month))
-       GROUP BY month;
+              (SELECT to_char(view_datetime, 'MON-YYYY'), COUNT(*) top FROM CASTS JOIN taps_movies ON CASTS.title=taps_movies.title  
+               GROUP BY to_char(view_datetime, 'MON-YYYY')
+               ORDER BY to_char(view_datetime, 'MON-YYYY'))
+       GROUP BY CASTS.actor, to_char(view_datetime, 'MON-YYYY');
        
               
-
+             
+             
+             
+             
+             
+             SELECT to_char(view_datetime, 'MON-YYYY'), ACTOR, rank() OVER (ORDER BY top desc) as mejor
+				FROM (SELECT COUNT(*) top
+				FROM CASTS JOIN taps_movies ON 
+				CASTS.title=taps_movies.title)  
+				 WHERE mejor = 1
+               GROUP BY ACTOR, to_char(view_datetime, 'MON-YYYY')
+               ORDER BY to_char(view_datetime, 'MON-YYYY');
+			   
+			
