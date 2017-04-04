cbc1c92b8d9a8c8bd80641e1e7900ba2b48e4cd8
gabrielglbh <100346140@alumnos.uc3m.es>
2017-04-04 12:34:39 +0200

    TERMINADA

--- ThirdQuery.sql
+++ ThirdQuery.sql
@@ -1,10 +1,10 @@
-SELECT name, surname, SEASONS.title, SEASONS.season
-  FROM ((SEASONS JOIN lic_series ON SEASONS.episodes=lic_series.episode)
-  JOIN CLIENTS ON clientId=client)
-  GROUP BY SEASONS.season HAVING max(episodes)=episode;
+SELECT CLIENTS.name, CLIENTS.surname, SEASONS.title, SEASONS.season
+  FROM (SEASONS JOIN lic_series ON SEASONS.title=lic_series.title AND SEASONS.season=lic_series.season 
+        JOIN CLIENTS ON clientId=client)
+  GROUP BY CLIENTS.name, CLIENTS.surname, SEASONS.title, SEASONS.season, SEASONS.episodes 
+  HAVING COUNT(lic_series.episode)=SEASONS.episodes;
   
-
-
+--2103--
   
 
 
