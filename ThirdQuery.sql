2d0da6325f1759fb6149ddef06fe1b4b739307a9
gabrielglbh <100346140@alumnos.uc3m.es>
2017-04-04 10:28:21 +0200

    Update ThirdQuery.sql

--- ThirdQuery.sql
+++ ThirdQuery.sql
@@ -1,7 +1,7 @@
 SELECT name, surname, SEASONS.title, SEASONS.season
   FROM (SEASONS JOIN lic_series ON SEASONS.season=lic_series.season)
-       JOIN clients ON clientId=client)
-  GROUP BY SEASONS.title HAVING COUNT(lic_series.episode)=max(SEASONS.episode);
+       JOIN clients ON clientId=client
+  GROUP BY episode HAVING COUNT(lic_series.episode)=SEASONS.episode;
   
 --SELECT COUNT(*) FROM (..);  
 
