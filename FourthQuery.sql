3eb3f90e1936a1ee4ae0966831c9b1afaa821029
francolmenar <correofalso97@hotmail.es>
2017-04-04 18:02:54 +0200

    Update FourthQuery.sql

--- FourthQuery.sql
+++ FourthQuery.sql
@@ -4,7 +4,7 @@
 		       FROM CASTS JOIN taps_movies ON CASTS.title=taps_movies.title  
 		       GROUP BY to_char(view_datetime, 'MON-YYYY'), actor 		   
 		       ORDER BY to_char(view_datetime, 'MON-YYYY'))
-	GROUP BY month;
+	GROUP BY month HAVING max(top)=188;
 			  
 			  
 			  
