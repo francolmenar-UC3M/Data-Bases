0bed1b996aee6254f2e05f9ec405557c9ff0cb82
gabrielglbh <100346140@alumnos.uc3m.es>
2017-04-04 11:56:44 +0200

    Update FirstQuery.sql

--- FirstQuery.sql
+++ FirstQuery.sql
@@ -4,7 +4,7 @@ SELECT clientId, name, surname, startdate, enddate, type
 	FROM clients
 	ORDER BY surname, name) 
 	NATURAL JOIN 
-	(SELECT startdate, enddate, contract_type, clientId
-	FROM contracts JOIN products ON product_name=contract_type
+	(SELECT startdate, enddate, type, clientId
+	FROM contracts JOIN products ON product_name=contract_type)
 	WHERE (sysdate<enddate AND sysdate>startdate) OR (sysdate >= startdate AND enddate IS NULL));
 --2258--
