54cbd24bd75c0a18b541afa3d0610ee70555f109
Gabriel García <100346140@alumnos.uc3m.es>
2017-04-04 14:10:11 +0200

    Update FourthQuery.sql

--- FourthQuery.sql
+++ FourthQuery.sql
@@ -1,7 +1,8 @@
-SELECT actor, month, maxT
-  select count(*)
-  FROM CASTS JOIN taps_movies ON CASTS.title=taps_movies.title
-  GROUP BY (actor, month)
+SELECT actor, month, MAX
+SELECT MAX(*) FROM (SELECT COUNT(*)
+       FROM CASTS JOIN taps_movies ON CASTS.title=taps_movies.title
+       (SELECT DATEPART(mm,view_datetime) AS month FROM taps_movies)
+       GROUP BY (actor, month));
 
 
-to_char()
+
