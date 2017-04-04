85b7ec7b60aa259b53dce69dbc03b64aabc7a194
francolmenar <correofalso97@hotmail.es>
2017-04-04 18:08:33 +0200

    Update FourthQuery.sql

--- FourthQuery.sql
+++ FourthQuery.sql
@@ -1,10 +1,10 @@
 
-	SELECT max(top), month FROM (
+	SELECT max(top), actor, month FROM (
 		SELECT to_char(view_datetime, 'MON-YYYY') AS month, actor, COUNT(CASTS.actor) top
 		       FROM CASTS JOIN taps_movies ON CASTS.title=taps_movies.title  
 		       GROUP BY to_char(view_datetime, 'MON-YYYY'), actor 		   
 		       ORDER BY to_char(view_datetime, 'MON-YYYY'))
-	GROUP BY month HAVING max(top)=188;
+	GROUP BY month, actor HAVING max(top)=188;
 			  
 			  
 			  
