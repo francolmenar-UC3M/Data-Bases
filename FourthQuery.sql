218d71f6dc17e0ad67b11cfd441b65cadfa4bbea
francolmenar <correofalso97@hotmail.es>
2017-04-04 18:24:44 +0200

    TERMINADA

--- FourthQuery.sql
+++ FourthQuery.sql
@@ -1,10 +1,20 @@
 
-	SELECT max(top), actor, month FROM (
-		SELECT to_char(view_datetime, 'MON-YYYY') AS month, actor, COUNT(CASTS.actor) top
-		       FROM CASTS JOIN taps_movies ON CASTS.title=taps_movies.title  
-		       GROUP BY to_char(view_datetime, 'MON-YYYY'), actor 		   
-		       ORDER BY to_char(view_datetime, 'MON-YYYY'))
-	GROUP BY month, actor HAVING max(top)=188;
+		SELECT max, month, actor FROM (
+		SELECT max(top) AS max, month FROM (
+			SELECT to_char(view_datetime, 'MON-YYYY') AS month, actor, COUNT(CASTS.actor) top
+				   FROM CASTS JOIN taps_movies ON CASTS.title=taps_movies.title  
+				   GROUP BY to_char(view_datetime, 'MON-YYYY'), actor 		   
+				   ORDER BY to_char(view_datetime, 'MON-YYYY'))
+					GROUP BY month)
+		NATURAL JOIN (
+			SELECT to_char(view_datetime, 'MON-YYYY') AS month, actor, COUNT(CASTS.actor) top1
+				   FROM CASTS JOIN taps_movies ON CASTS.title=taps_movies.title  
+				   GROUP BY to_char(view_datetime, 'MON-YYYY'), actor 		   
+				   ORDER BY to_char(view_datetime, 'MON-YYYY'))
+		WHERE top1=max;
+	
+	
+	--diciembre Morgan Freeman el resto DeNiro
 			  
 			  
 			  
