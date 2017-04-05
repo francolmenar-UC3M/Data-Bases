3507a4a8ac90669929f1fe5494bd251492f89f1f
francolmenar <correofalso97@hotmail.es>
2017-04-05 23:35:57 +0200

    Update jad.sql

--- jad.sql
+++ jad.sql
@@ -1,28 +1,29 @@
 DROP VIEW PredictTVseriesContract;
 DROP VIEW PredictTVseriesTaps;
+DROP VIEW PredictTVseriesTapsMax;
 
-CREATE VIEW PredictTVseriesContract AS
-	SELECT clientId, contractID 
-	FROM  CLIENTS NATURAL JOIN  contracts;	
 
+
+CREATE VIEW PredictTVseriesContract AS	
+SELECT clientId, contractID 	
+FROM  CLIENTS NATURAL JOIN  contracts;	
 
 
 CREATE VIEW PredictTVseriesTaps AS
-	SELECT clientId, title, episode, season
-	FROM  PredictTVseriesContract NATURAL JOIN  taps_series
-	WHERE clientID='15/73766815/37T';
+SELECT clientId, title, episode, season, view_datetime	
+FROM  PredictTVseriesContract 
+NATURAL JOIN  taps_series	
+WHERE clientID='15/73766815/37T';
+
+
+CREATE VIEW PredictTVseriesTapsMax AS	
+SELECT title, max(view_datetime) AS mostRecent	
+FROM  PredictTVseriesTaps 	
+GROUP BY title;
+
+
 	
-CREATE VIEW PredictTVseriesTapsMax AS
-	SELECT clientId, title, episode, season
-	FROM  PredictTVseriesTaps 
-	WHERE view_datetime= (SELECT max(view_datetime)
-		from taps_series
-		WHERE clientID='15/73766815/37T');
+SELECT * FROM PredictTVseriesContract;
 	
-	
-	SELECT max(view_datetime)
-		from taps_series
-		WHERE clientID='15/73766815/37T';
-	
-	SELECT * FROM PredictTVseriesContract;
-	SELECT * FROM PredictTVseriesTaps;
+SELECT * FROM PredictTVseriesTaps;
+SELECT * FROM PredictTVseriesTapsMax;	
