f04fbcdaae016af67b50be92666563c30ed23f7b
francolmenar <correofalso97@hotmail.es>
2017-04-08 14:41:42 +0200

    Add files via upload

--- function.sql
+++ function.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW TopContentMovie AS
-SELECT clientId, product_name1, tap_costMovies, tap_cost, month FROM(
-SELECT contractId, product_name AS product_name1,(tap_cost*2) as tap_costMovies,month FROM(
+SELECT clientId, product_name1, tap_costMovies, tap_cost, month, month1 FROM(
+SELECT clientId,contractId, product_name AS product_name1,(tap_cost*2) as tap_costMovies,month FROM(
 SELECT clientId, contractId, contract_type, month FROM(
 SELECT title,  contractId, to_char(view_datetime, 'MON-YYYY') AS month
 FROM taps_movies)
@@ -8,14 +8,15 @@ NATURAL JOIN contracts)
 JOIN products ON
 contract_type = product_name)
 JOIN (
-SELECT contractId AS contracts, product_name,tap_cost,month1 FROM(
+SELECT clientId as client1,contractId AS contracts, product_name,tap_cost,month1 FROM(
 SELECT clientId, contractId, contract_type, month1 FROM(
 SELECT title,  contractId, to_char(view_datetime, 'MON-YYYY') AS month1
 FROM taps_series)
 NATURAL JOIN contracts)
 JOIN products ON
 contract_type = product_name)
-ON contractId = contracts;
+ON (contractId = contracts
+  AND clientId = client1);
 
 
 
