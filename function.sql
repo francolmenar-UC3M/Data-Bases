1259d866b020cd0631546a8cf8e23db6ab5a50ab
francolmenar <correofalso97@hotmail.es>
2017-04-08 13:40:04 +0200

    Add files via upload

--- function.sql
+++ function.sql
@@ -1,8 +1,11 @@
 CREATE OR REPLACE VIEW TopContent AS
-SELECT clientId, product_name, tap_costs,month, view_datetime FROM(
-SELECT title,  contractId, to_char(view_datetime, 'MON-YYYY') AS month, view_datetime
+SELECT contractId, product_name,(tap_cost*2) as tap_costMovies,month FROM(
+SELECT clientId, contractId, contract_type, month FROM(
+SELECT title,  contractId, to_char(view_datetime, 'MON-YYYY') AS month
 FROM taps_movies)
-NATURAL JOIN contracts;
+NATURAL JOIN contracts)
+JOIN products ON
+contract_type = product_name;
 
 
-SELECT * FROM TopContent;s
+SELECT * FROM TopContent;
