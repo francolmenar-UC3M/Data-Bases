1f6db305ee4fa70fe398873f1d48e994b7472b32
francolmenar <correofalso97@hotmail.es>
2017-04-11 11:44:23 +0200

    Update TRIGGER D.sql

--- TRIGGER D.sql
+++ TRIGGER D.sql
@@ -8,3 +8,21 @@ BEGIN
 				DBMS_OUTPUT.PUT_LINE('Normal');
 		END IF;
 END triggerD;
+
+
+mini triger ara sabe como va la vaina
+CREATE OR REPLACE TRIGGER triggerD BEFORE INSERT ON contracts FOR EACH ROW 		
+DECLARE
+	t_enddate contracts.enddate%TYPE;
+	t_startdate contracts.startdate%TYPE;
+BEGIN
+	select enddate into t_enddate from contracts;
+	select startdate into t_startdate from contracts;
+	IF t_enddate > :new.startdate AND t_startdate <= sysdate THEN
+				DBMS_OUTPUT.PUT_LINE('joder');				
+	END IF;
+END triggerD;
+/
+show errors;
+
+
