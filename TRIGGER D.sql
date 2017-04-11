3823d5f2942e4e32719d3113d8b087b969402f6b
francolmenar <correofalso97@hotmail.es>
2017-04-11 13:31:24 +0200

    LEE LA DESCRIPCION CABRON
    Si quitas los puos checks de null y metes el primer insert sin que este el puto trigger y despues metes el trigger y el insert segundo el trigger va e imprme joder creo. El problema esta al insertarlo por primera vez

--- TRIGGER D.sql
+++ TRIGGER D.sql
@@ -10,8 +10,7 @@ BEGIN
 END triggerD;
 
 
-mini triger ara sabe como va la vaina
-DELETE FROM CONTRACTS WHERE clientId='Timmi';
+mini triger ara sabe como va la vainaDELETE FROM CONTRACTS WHERE clientId='Timmi';
 DELETE FROM Clients WHERE clientId='Timmi';
 DELETE FROM CONTRACTS WHERE contractId='Timmi1';
 DROP trigger triggerD;
@@ -25,17 +24,39 @@ CREATE OR REPLACE TRIGGER triggerD BEFORE INSERT ON contracts FOR EACH ROW
 DECLARE
 	t_enddate contracts.enddate%TYPE;
 	t_startdate contracts.startdate%TYPE;
+	noHayEnd number(1);
+	noHayStart number(1);
 BEGIN
+	SET noHayEnd =
+	CASE WHEN NOT EXISTS (select enddate From contracts 
+	where contracts.clientId = :new.clientId) THEN 1
+	ELSE 0
+	end case;
+
+	SET noHayStart =
+	CASE WHEN EXISTS (select startdate from contracts 	
+	where contracts.clientId = :new.clientId) THEN noHayStart = 1
+	ELSE
+ 0	
+	END case;
+
+	IF noHayEnd == 0 AND noHayStart == 0 then
 	select enddate into t_enddate 
 	from contracts 
 	where contracts.clientId = :new.clientId;
 	select startdate into t_startdate 
 	from contracts 
 	where contracts.clientId = :new.clientId;
-	IF t_enddate IS NULL AND t_startdate IS NULL THEN
+
+		IF t_enddate IS NULL AND t_startdate IS NULL THEN
 		DBMS_OUTPUT.PUT_LINE('Puto Timmi');
-	ELSIF t_enddate > :new.startdate AND t_startdate <= sysdate THEN
+	
+		ELSIF t_enddate > :new.startdate AND t_startdate <= sysdate THEN
 				DBMS_OUTPUT.PUT_LINE('joder');			
+		END IF;
+	ELSE
+	DBMS_OUTPUT.PUT_LINE('nO HAY NADA PRIMO');	
+
 	END IF;
 END triggerD;
 /
