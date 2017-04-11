7964f0700443c6484acd4cfda80fe488c2a5c500
francolmenar <correofalso97@hotmail.es>
2017-04-11 12:58:20 +0200

    Update TRIGGER D.sql

--- TRIGGER D.sql
+++ TRIGGER D.sql
@@ -11,6 +11,16 @@ END triggerD;
 
 
 mini triger ara sabe como va la vaina
+DELETE FROM CONTRACTS WHERE clientId='Timmi';
+DELETE FROM Clients WHERE clientId='Timmi';
+DELETE FROM CONTRACTS WHERE contractId='Timmi1';
+DROP trigger triggerD;
+
+INSERT INTO CLIENTS VALUES('Timmi','Timmi','Timmi','Timmi','Timmi','tuputamadre@hotmail.com',1,TO_DATE('10-OCT-13', 'YYYY-MM-DD'));
+
+insert into contracts VALUES('Timmi','Timmi',TO_DATE('10-OCT-13', 'YYYY-MM-DD'),TO_DATE('10-OCT-19', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
+
+
 CREATE OR REPLACE TRIGGER triggerD BEFORE INSERT ON contracts FOR EACH ROW 		
 DECLARE
 	t_enddate contracts.enddate%TYPE;
@@ -18,10 +28,10 @@ DECLARE
 BEGIN
 	select enddate into t_enddate 
 	from contracts 
-	where contracts.contractId = :new.contractId;
+	where contracts.clientId = :new.clientId;
 	select startdate into t_startdate 
 	from contracts 
-	where contracts.contractId = :new.contractId;
+	where contracts.clientId = :new.clientId;
 	IF t_enddate IS NULL AND t_startdate IS NULL THEN
 		DBMS_OUTPUT.PUT_LINE('Puto Timmi');
 	ELSIF t_enddate > :new.startdate AND t_startdate <= sysdate THEN
@@ -31,11 +41,10 @@ END triggerD;
 /
 show errors;
 
-insert into contracts VALUES('Timmi','00/98607278/50T',TO_DATE('10-OCT-13', 'YYYY-MM-DD'),TO_DATE('10-OCT-19', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
 
-insert into contracts VALUES('Timmi1','00/98607278/50T',TO_DATE('10-OCT-14', 'YYYY-MM-DD'),TO_DATE('10-OCT-18', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
+insert into contracts VALUES('Timmi1','Timmi',TO_DATE('10-OCT-14', 'YYYY-MM-DD'),TO_DATE('10-OCT-18', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
 
-SELECT * FROM CONTRACTS WHERE contractId='Timmi';
-DELETE FROM CONTRACTS WHERE contractId='Timmi';
 
-DROP trigger triggerD;
+SELECT * FROM CONTRACTS WHERE contractId='Timmi1';
+SELECT * FROM CONTRACTS WHERE clientId='Timmi';
+
