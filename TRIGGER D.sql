fdc39569eee748c74ed6bed5072ab20263226186
francolmenar <correofalso97@hotmail.es>
2017-04-11 12:38:44 +0200

    Update TRIGGER D.sql

--- TRIGGER D.sql
+++ TRIGGER D.sql
@@ -16,8 +16,12 @@ DECLARE
 	t_enddate contracts.enddate%TYPE;
 	t_startdate contracts.startdate%TYPE;
 BEGIN
-	select enddate into t_enddate from contracts;
-	select startdate into t_startdate from contracts;
+	select enddate into t_enddate 
+	from contracts 
+	where contracts.contractId = :new.contractId;
+	select startdate into t_startdate 
+	from contracts 
+	where contracts.contractId = :new.contractId;
 	IF t_enddate IS NULL AND t_startdate IS NULL THEN
 		DBMS_OUTPUT.PUT_LINE('Puto Timmi');
 	ELSIF t_enddate > :new.startdate AND t_startdate <= sysdate THEN
@@ -29,5 +33,9 @@ show errors;
 
 insert into contracts VALUES('Timmi','00/98607278/50T',TO_DATE('10-OCT-13', 'YYYY-MM-DD'),TO_DATE('10-OCT-19', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
 
+insert into contracts VALUES('Timmi1','00/98607278/50T',TO_DATE('10-OCT-14', 'YYYY-MM-DD'),TO_DATE('10-OCT-18', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
+
 SELECT * FROM CONTRACTS WHERE contractId='Timmi';
 DELETE FROM CONTRACTS WHERE contractId='Timmi';
+
+DROP trigger triggerD;
