d4adc91f09c21d93122d517ee17ed28c0ec6eaf0
francolmenar <correofalso97@hotmail.es>
2017-04-12 16:21:36 +0200

    Update TRIGGER D.sql

--- TRIGGER D.sql
+++ TRIGGER D.sql
@@ -1,12 +1,24 @@
-DELETE FROM CONTRACTS WHERE clientId='Timmi';
+CREATE OR REPLACE TRIGGER triggerD 
+BEFORE INSERT ON contracts FOR EACH ROW 		
+BEGIN
+		IF (enddate > :new.startdate) AND (startdate <= sysdate) THEN
+					UPDATE contracts
+					SET enddate := :new.startdate-1
+		ELSE
+				DBMS_OUTPUT.PUT_LINE('Normal');
+		END IF;
+END triggerD;
+
+
+mini triger ara sabe como va la vainaDELETE FROM CONTRACTS WHERE clientId='Timmi';
 DELETE FROM Clients WHERE clientId='Timmi';
+DELETE FROM CONTRACTS WHERE contractId='Timmi';
 DELETE FROM CONTRACTS WHERE contractId='Timmi1';
 DROP trigger triggerD;
 
 INSERT INTO CLIENTS VALUES('Timmi','Timmi','Timmi','Timmi','Timmi','tuputamadre@hotmail.com',1,TO_DATE('10-OCT-13', 'YYYY-MM-DD'));
 
 insert into contracts VALUES('Timmi','Timmi',TO_DATE('10-OCT-13', 'YYYY-MM-DD'),TO_DATE('10-OCT-19', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
-
 
 
 CREATE OR REPLACE TRIGGER triggerD BEFORE INSERT ON contracts FOR EACH ROW 		
@@ -49,19 +61,28 @@ BEGIN
 	where contracts.clientId = :new.clientId;
 
 		IF t_enddate > :new.startdate AND t_startdate <= sysdate THEN
-				DBMS_OUTPUT.PUT_LINE('HAY QUE CURRAR');	
+				DBMS_OUTPUT.PUT_LINE('DIA MENOS UNO');	
 					t_enddate := :new.startdate-1;	
-		END IF;	
-	ELSE
-		DBMS_OUTPUT.PUT_LINE('MIRAR CASOS');	
+		END IF;
+	ELSIF 	(noHayStart = 0) then
+		DBMS_OUTPUT.PUT_LINE('INSERT NORMAL PAYO TONTO');	
 		
+	ELSE
+		DBMS_OUTPUT.PUT_LINE('PAYO CON CONTRATO PERO SIN ENDDATE');	
+		
+		select startdate into t_startdate 
+		from contracts 
+		where contracts.clientId = :new.clientId;
 
+			IF t_startdate <= sysdate THEN
+			DBMS_OUTPUT.PUT_LINE('DIA MENOS UNO(sin end');	
+					t_enddate := :new.startdate-1;
+			end if;
+	
 	END IF;
 END triggerD;
 /
 show errors;
-
-
 
 
 insert into contracts VALUES('Timmi1','Timmi',TO_DATE('10-OCT-14', 'YYYY-MM-DD'),TO_DATE('10-OCT-18', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
@@ -69,4 +90,3 @@ insert into contracts VALUES('Timmi1','Timmi',TO_DATE('10-OCT-14', 'YYYY-MM-DD')
 
 SELECT * FROM CONTRACTS WHERE contractId='Timmi1';
 SELECT * FROM CONTRACTS WHERE clientId='Timmi';
-
