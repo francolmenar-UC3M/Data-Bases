95e7875a00a6cf11d778d1ffaf7d50dc4dff6c24
francolmenar <correofalso97@hotmail.es>
2017-04-17 18:00:58 +0200

    Update TRIGGER D.sql

--- TRIGGER D.sql
+++ TRIGGER D.sql
@@ -1,24 +1,12 @@
-CREATE OR REPLACE TRIGGER triggerD 
-BEFORE INSERT ON contracts FOR EACH ROW 		
-BEGIN
-		IF (enddate > :new.startdate) AND (startdate <= sysdate) THEN
-					UPDATE contracts
-					SET enddate := :new.startdate-1
-		ELSE
-				DBMS_OUTPUT.PUT_LINE('Normal');
-		END IF;
-END triggerD;
-
-
-mini triger ara sabe como va la vainaDELETE FROM CONTRACTS WHERE clientId='Timmi';
+delete from taps_movies where contractId='Timmi';
 DELETE FROM Clients WHERE clientId='Timmi';
 DELETE FROM CONTRACTS WHERE contractId='Timmi';
 DELETE FROM CONTRACTS WHERE contractId='Timmi1';
-DROP trigger triggerD;
 
 INSERT INTO CLIENTS VALUES('Timmi','Timmi','Timmi','Timmi','Timmi','tuputamadre@hotmail.com',1,TO_DATE('10-OCT-13', 'YYYY-MM-DD'));
 
-insert into contracts VALUES('Timmi','Timmi',TO_DATE('10-OCT-13', 'YYYY-MM-DD'),TO_DATE('10-OCT-19', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
+insert into contracts VALUES('Timmi','Timmi',TO_DATE('2010-03-13', 'YYYY-MM-DD'),TO_DATE('2010-03-19', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
+insert into contracts VALUES('Timmi1','Timmi',TO_DATE('2010-03-16', 'YYYY-MM-DD'),TO_DATE('2010-03-19', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
 
 
 CREATE OR REPLACE TRIGGER triggerD BEFORE INSERT ON contracts FOR EACH ROW 		
@@ -28,8 +16,6 @@ DECLARE
 	noHayEnd number(1);
 	noHayStart number(1);
 	cnt number;
-
-	
 BEGIN
 	
 	SELECT COUNT(*) INTO noHayEnd FROM (select enddate From contracts 
@@ -45,11 +31,10 @@ BEGIN
 	where contracts.clientId = :new.clientId);
 
 	IF(noHayStart = 0) THEN
-	DBMS_OUTPUT.PUT_LINE('No hay noHayStart');	
+	DBMS_OUTPUT.PUT_LINE('No hay Start date');	
 	else
-	DBMS_OUTPUT.PUT_LINE('hay noHayStart');	
+	DBMS_OUTPUT.PUT_LINE('hay Start date');	
 	END IF;
-
 	
 	IF(noHayStart = 1 AND noHayEnd = 1) then
 	select enddate into t_enddate 
@@ -65,18 +50,18 @@ BEGIN
 					t_enddate := :new.startdate-1;	
 		END IF;
 	ELSIF 	(noHayStart = 0) then
-		DBMS_OUTPUT.PUT_LINE('INSERT NORMAL PAYO TONTO');	
+		DBMS_OUTPUT.PUT_LINE('INSERT NORMAL');	
 		
 	ELSE
-		DBMS_OUTPUT.PUT_LINE('PAYO CON CONTRATO PERO SIN ENDDATE');	
+		DBMS_OUTPUT.PUT_LINE('CON CONTRATO PERO SIN ENDDATE');	
 		
 		select startdate into t_startdate 
 		from contracts 
 		where contracts.clientId = :new.clientId;
 
 			IF t_startdate <= sysdate THEN
-			DBMS_OUTPUT.PUT_LINE('DIA MENOS UNO(sin end');	
-					t_enddate := :new.startdate-1;
+			DBMS_OUTPUT.PUT_LINE('DIA MENOS UNO(sin end)');	
+					insert enddate := :new.startdate-1;
 			end if;
 	
 	END IF;
@@ -85,8 +70,5 @@ END triggerD;
 show errors;
 
 
-insert into contracts VALUES('Timmi1','Timmi',TO_DATE('10-OCT-14', 'YYYY-MM-DD'),TO_DATE('10-OCT-18', 'YYYY-MM-DD'),'Short Timer','payo','tonto','matame','ya');
-
-
 SELECT * FROM CONTRACTS WHERE contractId='Timmi1';
-SELECT * FROM CONTRACTS WHERE clientId='Timmi';
+SELECT * FROM CONTRACTS WHERE contractId='Timmi';
