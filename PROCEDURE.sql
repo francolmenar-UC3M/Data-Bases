0e3c180b3e2a9702f7c788cb2aa725c077d52e9d
francolmenar <correofalso97@hotmail.es>
2017-04-20 15:51:28 +0200

    TERMINADO

--- PROCEDURE.sql
+++ PROCEDURE.sql
@@ -1,6 +1,8 @@
 CREATE OR REPLACE PROCEDURE billTotal (monthInput VARCHAR2) 
 IS
-		counter NUMBER;
+		counter NUMBER(8,2);
+		month NUMBER(2);
+		year NUMBER(4);
 
 CURSOR rigthContracts(monthInput VARCHAR2) IS 
  SELECT clientId,contractId, name, surname, startdate, enddate, product_name FROM (
@@ -17,14 +19,13 @@ BEGIN
 		IF rigthContracts %ISOPEN THEN
 				CLOSE rigthContracts;
 			END IF;
+		month := extract(month from (TO_DATE(monthInput,'MM-YYYY')));
+		year := extract(year from (TO_DATE(monthInput,'MM-YYYY')));
 		FOR aux_user IN rigthContracts(monthInput)
 		LOOP
 			counter := bill(aux_user.clientId,monthInput,aux_user.product_name);
-			INSERT INTO invoices(aux.contra
-			DBMS_OUTPUT.PUT_LINE('The client  '|| aux_user.clientId || ' has a bill of ' || counter || ' of ' || monthInput);
+			INSERT INTO invoices VALUES(aux_user.contractId,month,year,SYSDATE,counter);
 		END LOOP;
-			
-		counter := 0;
 END;
 /
 show errors;
@@ -34,6 +35,7 @@ begin
 billTotal('NOV-10');
 end;
 /
+
 
 TEST
 IF aux_user.clientId = 'Timmi' THEN 
