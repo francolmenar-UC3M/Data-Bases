05f4aa123297b47aa4749222ab271761babfe43a
Gabriel García <100346140@alumnos.uc3m.es>
2017-05-04 16:49:41 +0200

    METER

--- Tercero/clusters.sql
+++ Tercero/clusters.sql
@@ -3,9 +3,9 @@ DROP CLUSTER cliente;
 --DROP CLUSTER series;
 
 DROP INDEX t_movie;
-DROP INDEX cliente;
+DROP INDEX t_cliente;
 DROP INDEX i_sed;
-DROP INDEX series;
+--DROP INDEX series;
 
 DROP TABLE MOVIES CASCADE CONSTRAINTS;
 DROP TABLE GENRES_MOVIES CASCADE CONSTRAINTS;
@@ -182,7 +182,7 @@ title VARCHAR2(100) NOT NULL,
 CONSTRAINT PK_licsM PRIMARY KEY (client,title),
 CONSTRAINT FK_licsM1 FOREIGN KEY (title) REFERENCES movies,
 CONSTRAINT FK_licsM2 FOREIGN KEY (client) REFERENCES clients ON DELETE CASCADE
-)CLUSTER cliente (client);
+);
 
 
 CREATE TABLE lic_series(
@@ -194,7 +194,7 @@ episode NUMBER(3) NOT NULL,
 CONSTRAINT PK_licsS PRIMARY KEY (client,title,season,episode),
 CONSTRAINT FK_licsS1 FOREIGN KEY (title,season) REFERENCES seasons,
 CONSTRAINT FK_licsS2 FOREIGN KEY (client) REFERENCES clients ON DELETE CASCADE
-)CLUSTER cliente (client);
+);
 
 
 CREATE TABLE invoices(
