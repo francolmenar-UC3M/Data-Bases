3b9dc04c5cd22662adb74cec068fb4f60456f180
Gabriel García <100346140@alumnos.uc3m.es>
2017-05-06 21:37:08 +0200

    385K

--- Tercero/clusters.sql
+++ Tercero/clusters.sql
@@ -223,13 +223,13 @@ CREATE INDEX t_contract ON CLUSTER contract;
 CREATE INDEX t_movie ON CLUSTER titleM;
 CREATE INDEX lic_movie ON lic_movies(client, title, datetime); 
 CREATE INDEX lic_serie ON lic_series(client, title, season, episode, datetime); 
-
 CREATE INDEX i_sed ON contracts(startdate, enddate, clientId);
---CREATE INDEX name ON clients(name, surname, sec_surname); 
---CREATE INDEX cry ON movies (country, duration);
---CREATE INDEX series ON seasons(title, episodes, season); 
+CREATE INDEX name ON clients(name, surname, sec_surname); 
+
 --CREATE INDEX viewsM ON taps_movies(view_datetime, pct); 
 --CREATE INDEX viewsS ON taps_series(view_datetime, pct); 
+--CREATE INDEX cry ON movies (country, duration);
+--CREATE INDEX series ON seasons(title, episodes, season); 
 --CREATE INDEX dur ON seasons (avgduration); 
 
 --CREATE INDEX i_sed ON contracts(startdate, enddate) TABLESPACE tabsp_2k;
